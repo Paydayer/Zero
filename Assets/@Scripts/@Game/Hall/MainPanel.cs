088c6f00008dac876e1d19931e1fc0efefc374a4
DESKTOP-VREE439\Administrator <jxu3d06@hotmail.com>
2025-07-11 16:10:51 +0800

    最新操作

--- Assets/@Scripts/@Game/Hall/MainPanel.cs
+++ Assets/@Scripts/@Game/Hall/MainPanel.cs
@@ -1,15 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Zero;
 using ZeroGameKit;
 public class MainPanel : AView
 {
     public MainCell Image;
     public Transform obj;
+    public Text txtName;
     protected override void OnEnable()
     {
        Debug.Log("11111222");
+       txtName.text = "狠狠的热";
        Image.Show();
        //Debug.Log(transform.GetComponent<MainPanel>());
        Debug.Log(obj.name);
